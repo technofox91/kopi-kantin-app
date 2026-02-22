1063d4fb0d713afef37b39069f02d6c239d500ec
Ignatius Adhitya <ignatiusadhitya@Ignatiuss-MacBook-Air.local>
2026-02-22 23:03:36 +0700

    Fix: Add type for children in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,13 @@
 import './globals.css'
 
-export default function RootLayout({ children }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="antialiased">{children}</body>
     </html>
   )
 }
